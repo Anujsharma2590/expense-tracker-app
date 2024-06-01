90f1937d4134efc589beffdfcd797e63dc1f48ed
Anuj sharma <aanuj.sharma2590@gmail.com>
2024-06-01 20:29:10 +0530

    add swagger file

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
-
 import userRouter from "./routes/user.js";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocument from "./docs/openapi.json" assert { type: "json" };
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use(userRouter);
 
 app.listen(8800, () => {
